6bab36e11a698b0b62edec43232158613d7faf13
Guzman Monne <guzmonne@hotmail.com>
2018-01-21 11:05:41 -0300

    improved the Brush functionality

--- src/App/ResponsiveLineChart/Brush/Brush.js
+++ src/App/ResponsiveLineChart/Brush/Brush.js
@@ -7,15 +7,23 @@ import toClass from 'recompose/toClass.js';
 import { PADDING } from '../constants.js';
 import { scaleQuantize } from 'd3-scale';
 
-//     ref={c => (this.svg = c)}
-
-var Brush = ({ width, height, minEdge, maxEdge, onMouseDown, onMouseMove }) => {
+var Brush = ({
+  width,
+  height,
+  minEdge,
+  maxEdge,
+  onMouseDown,
+  onMouseMove,
+  onMouseUp
+}) => {
   return (
     <svg
       ref={c => (this.svg = c)}
       width={width}
       height={height}
       onMouseMove={onMouseMove}
+      onMouseUp={onMouseUp}
+      onMouseLeave={onMouseUp}
     >
       <rect
         x={0}
@@ -24,7 +32,7 @@ var Brush = ({ width, height, minEdge, maxEdge, onMouseDown, onMouseMove }) => {
         height={height}
         style={{
           opacity: 0.2,
-          fill: 'red'
+          fill: 'transparent'
         }}
         onMouseDown={onMouseDown('new')}
       />
@@ -39,6 +47,30 @@ var Brush = ({ width, height, minEdge, maxEdge, onMouseDown, onMouseMove }) => {
         }}
         onMouseDown={onMouseDown('both')}
       />
+      <rect
+        x={minEdge}
+        y={0}
+        width={PADDING}
+        height={height}
+        style={{
+          opacity: 0.5,
+          fill: 'black',
+          cursor: 'w-resize'
+        }}
+        onMouseDown={onMouseDown('minEdge')}
+      />
+      <rect
+        x={maxEdge - PADDING}
+        y={0}
+        width={PADDING}
+        height={height}
+        style={{
+          opacity: 0.5,
+          fill: 'black',
+          cursor: 'e-resize'
+        }}
+        onMouseDown={onMouseDown('maxEdge')}
+      />
     </svg>
   );
 };
@@ -50,28 +82,32 @@ var enhance = compose(
 
       return {
         minEdge: range[0],
-        maxEdge: range[1] / 2,
-        dragging: undefined,
+        maxEdge: range[1],
+        dragging: false,
         difference: 0
       };
     },
     {
-      setDifference: () => difference => ({ difference }),
-      setDragging: () => dragging => ({ dragging }),
-      setEdges: ({ minEdge, maxEdge }, { xScale }) => (
-        newMinEdge,
-        newMaxEdge
-      ) => {
+      setState: (
+        { minEdge: _minEdge, maxEdge: _maxEdge, dragging },
+        { xScale }
+      ) => ({ minEdge, maxEdge, difference = 0, dragging = false }) => {
         var state = {
-          minEdge: newMinEdge >= 0 ? newMinEdge : minEdge,
-          maxEdge: newMaxEdge <= xScale.range()[1] ? newMinEdge : maxEdge
+          difference,
+          dragging,
+          minEdge: minEdge !== undefined ? minEdge : _minEdge,
+          maxEdge: maxEdge !== undefined ? maxEdge : _maxEdge
         };
 
-        if (state.minEdge > state.maxEdge)
-          state = {
-            minEdge: state.maxEdge,
-            maxEdge: state.minEdge
-          };
+        if (state.maxEdge > xScale.range()[1]) {
+          state.maxEdge = xScale.range()[1];
+          state.minEdge = _minEdge;
+        }
+
+        if (state.minEdge < 0) {
+          state.minEdge = 0;
+          state.maxEdge = _maxEdge;
+        }
 
         return state;
       }
@@ -83,22 +119,75 @@ var enhance = compose(
       pt.x = clientX;
       pt.y = clientY;
       var result = pt.matrixTransform(svg.getScreenCTM().inverse());
-      console.log(result);
       return result;
     }
   }),
   withHandlers({
-    onMouseDown: ({ cursorPoint, setDragging, setEdges }) => side => e => {
+    onMouseDown: ({ cursorPoint, setState }) => side => e => {
       var { x } = cursorPoint(this.svg, e.clientX, e.clientY);
 
-      setDragging(side === 'min' || side === 'max' ? side : x);
+      var state = { dragging: x };
 
-      if (side === 'new') setEdges(x, x);
+      if (side === 'minEdge' || side === 'maxEdge') state.dragging = side;
+
+      if (side === 'new') {
+        state.minEdge = x;
+        state.maxEdge = x;
+        state.dragging = 'minEdge';
+      }
+
+      return setState(state);
     },
-    onMouseMove: ({ dragging }) => e => {
+    onMouseMove: ({
+      minEdge,
+      maxEdge,
+      dragging,
+      difference,
+      cursorPoint,
+      setState
+    }) => e => {
       // Do nothing if not dragging.
-      if (dragging === undefined) return;
-    }
+      if (dragging === false) return;
+
+      var { x } = cursorPoint(this.svg, e.clientX, e.clientY);
+
+      var state = {
+        minEdge,
+        maxEdge,
+        difference,
+        dragging
+      };
+
+      if (typeof dragging === 'number') {
+        // We need to store the difference to substract it on further calls.
+        state.difference = dragging - x;
+        state.minEdge = minEdge - (state.difference - difference);
+        state.maxEdge = maxEdge - (state.difference - difference);
+      } else {
+        // Store the new dragging value
+        state[dragging] = x;
+
+        if (dragging === 'maxEdge' && x <= minEdge) {
+          state.minEdge = maxEdge;
+          state.maxEdge = minEdge;
+          state.dragging = 'minEdge';
+        }
+        if (dragging === 'minEdge' && x >= maxEdge) {
+          state.minEdge = maxEdge;
+          state.maxEdge = minEdge;
+          state.dragging = 'maxEdge';
+        }
+      }
+      // Flip max and min if their difference is smaller than zero.
+      if (state.minEdge > state.maxEdge) {
+        let tmp = state.maxEdge;
+        state.maxEdge = state.minEdge;
+        state.minEdge = tmp;
+      }
+
+      setState(state);
+    },
+    onMouseUp: ({ setState }) => setState
   }),
   toClass,
   pure
@@ -108,150 +197,3 @@ var EnhancedBrush = enhance(Brush);
 EnhancedBrush.displayName = 'enhance(Brush)';
 
 export default EnhancedBrush;
-
-class BrushOld extends React.Component {
-  state = {
-    min: 0,
-    max: 0,
-    dragging: undefined,
-    difference: 0
-  };
-
-  cursorPoint = (clientX, clientY) => {
-    this.pt || (this.pt = this.pt = this.svg.createSVGPoint());
-    this.pt.x = clientX;
-    this.pt.y = clientY;
-    return this.pt.matrixTransform(this.svg.getScreenCTM().inverse());
-  };
-
-  handleOnMouseDown = side => e => {
-    // Moving the entire selection
-    if (side === 'both')
-      this.setState({ dragging: this.cursorPoint(e.clientX, e.clientY).x });
-    else if (side === 'new') {
-      // Creating a new selection
-      var x = this.cursorPoint(e.clientX, e.clientY).x;
-      this.setState({ dragging: 'min', min: x, max: x });
-      // Moving a side of the selection
-    } else this.setState({ dragging: side });
-  };
-
-  handleOnMouseMove = e => {
-    var { dragging } = this.state;
-    // Do nothing if not dragging.
-    if (dragging === undefined) return;
-
-    var { max, min, difference } = this.state;
-    var { width } = this.props;
-    var { x } = this.cursorPoint(e.clientX, e.clientY);
-
-    var result = {};
-
-    if (typeof dragging === 'number') {
-      // We need to store the difference to substract it on further calls.
-      result.difference = dragging - x;
-      result.min = min - (result.difference - difference);
-      result.max = max - (result.difference - difference);
-    } else {
-      // Store the new dragging value
-      result[dragging] = x;
-
-      if (dragging === 'max' && x <= min) {
-        result.min = max;
-        result.max = min;
-        result.dragging = 'min';
-      }
-      if (dragging === 'min' && x >= max) {
-        result.min = max;
-        result.max = min;
-        result.dragging = 'max';
-      }
-    }
-    // Flip max and min if their difference is smaller than zero.
-    if (result.min > result.max) {
-      result.max = result.min;
-      result.min = result.max;
-    }
-    // Fix the selection in place if the min value is lower than zero.
-    if (result.min < 0) {
-      result.min = 0;
-      result.max = max;
-    }
-    // Fix the selection in place if the max value is higher than the width.
-    if (result.max > width) {
-      result.min = min;
-      result.max = width;
-    }
-
-    return this.setState(result, () => {
-      if (typeof this.props.onBrush === 'function') {
-        this.props.onBrush(this.state.min, this.state.max);
-      }
-    });
-  };
-
-  handleOnMouseUp = e => {
-    this.setState({ dragging: undefined, difference: 0 });
-  };
-
-  render() {
-    var { min, max } = this.state;
-    var { width, height } = this.props;
-
-    return (
-      <svg
-        ref={c => (this.svg = c)}
-        width={width}
-        height={height}
-        onMouseMove={this.handleOnMouseMove}
-        onMouseUp={this.handleOnMouseUp}
-        onMouseLeave={this.handleOnMouseUp}
-      >
-        <rect
-          x={0}
-          y={0}
-          width={width}
-          height={height}
-          style={{
-            fill: 'transparent'
-          }}
-          onMouseDown={this.handleOnMouseDown('new')}
-        />
-        <rect
-          x={min}
-          y={0}
-          width={max - min}
-          height={height}
-          style={{
-            fill: 'black',
-            opacity: 0.3,
-            cursor: 'col-resize'
-          }}
-          onMouseDown={this.handleOnMouseDown('both')}
-        />
-        <rect
-          x={min}
-          y={0}
-          width={PADDING}
-          height={height}
-          style={{
-            fill: 'transparent',
-            cursor: 'w-resize'
-          }}
-          onMouseDown={this.handleOnMouseDown('min')}
-        />
-        <rect
-          x={max - PADDING}
-          y={0}
-          width={PADDING}
-          height={height}
-          style={{
-            fill: 'transparent',
-            cursor: 'e-resize'
-          }}
-          onMouseDown={this.handleOnMouseDown('max')}
-        />
-      </svg>
-    );
-  }
-}
